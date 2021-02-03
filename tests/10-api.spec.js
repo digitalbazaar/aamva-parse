52746b4ec45de5804247968b7d0df4b957cf1aac
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2021-02-03 13:38:55 -0500

    Add test for Florida issued license.

--- tests/10-api.spec.js
+++ tests/10-api.spec.js
@@ -11,6 +11,10 @@ const sample1 = `@\n\u001e\rANSI 636000030001DL00310447DLDCADM2 \nDCBNONE      \
 // eslint-disable-next-line max-len
 const sample2 = `@\n\u001e\rANSI 636000030001DL00310440DLDCAM2  \nDCBNONE      \nDCDNONE \nDBA12302020\nDCSBROWN                                 \nDCTMATTHEW,ALAN                                                                    \nDBD08272014\nDBB12301970\nDBC1\nDAYGRN\nDAU072 in\nDAG110 OAK ST                     \nDAISTAUNTON            \nDAJVA\nDAK244014120  \nDAQT65903333                \nDCF072655761                \nDCGUSA\nDCHNONE\nDDC00000000\nDDB12102008\nDDDN\nDDAN\nDCK0060100726807477         \n\r`;
 
+// Florida issued 2018
+// eslint-disable-next-line max-len
+const sample3 = `@\n\u001e\rANSI 636010090002DL00410257ZF02980067DLDAQX485012001000  \nDCSJOHN  \nDDEN  \nDACDOE  \nDDFN  \nDADORI  \nDDGN  \nDCAE  \nDCBA  \nDCDNONE  \nDBD08262018  \nDBB08301980 \nDBA01102027 \nDBC2  \nDAU065 IN\nDAG1208 C HILL AVE 822  \nDAITAMPA  \nDAJFL  \nDAK345026628  \nDCFK711568218311  \nDCGUSA  \nDCK0109328184918218\nDDAF\nDDB03162017\nDDK1\rZFZFA\nZFB\nZFCSAFE DRIVER\nZFD\nZFE\nZFF\nZFG\nZFH\nZFI\nZFJ0218157266\nZFK\r`;
+
 describe('parse test', () => {
   it('parses sample1', async () => {
     const t = parse({text: sample1});
@@ -26,6 +30,14 @@ describe('parse test', () => {
       dob: '12301970',
       docId: 'T65903333',
       expiration: '12302020'
+    });
+  });
+  it('parses sample3', async () => {
+    const t = parse({text: sample3});
+    t.should.eql({
+      dob: '08301980',
+      docId: 'X485012001000',
+      expiration: '01102027'
     });
   });
 });
